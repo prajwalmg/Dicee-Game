56218e949346b5a9d7f4dc95ce76a45644ba5e66
Prajwal MG <prajwalmg13@gmail.com>
2021-07-19 12:30:37 +0530

    Initial Commit including gitignore

---
+++ index.js
@@ -0,0 +1,15 @@
+
+var dicePics = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+var randomNumber1 = Math.floor(Math.random() * 6);
+var randomNumber2 = Math.floor(Math.random() * 6);
+document.querySelector(".img1").setAttribute("src", dicePics[randomNumber1]);
+document.querySelector(".img2").setAttribute("src", dicePics[randomNumber2]);
+if(randomNumber1===randomNumber2){
+  document.querySelector("h1").innerHTML = "Draw!";
+}
+else if (randomNumber1>randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player1 wins!";
+}
+else{
+  document.querySelector("h1").innerHTML = "Player2 wins!";
+}
